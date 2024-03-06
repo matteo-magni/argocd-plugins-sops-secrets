ec3d9709b996a43486ae11aa1603ce0e03eb9c60
Matteo Magni <matteo.magni@gmail.com>
2024-03-06 17:24:51 +0100

    fix sops-secrets-bin files paths in sops-secrets.sh script

--- sops-secrets-bin/sops-secrets.sh
+++ sops-secrets-bin/sops-secrets.sh
@@ -11,25 +11,25 @@ CWD=$(realpath $(dirname $0))
 
 if [[ "${PARAM_MODE}" == "helm" ]]; then
 
-    ${CWD}/sops-secrets-bin-helm.sh
+    ${CWD}/helm.sh
 
 elif [[ "${PARAM_MODE}" == "ytt" ]]; then
     # this is a ytt templated directory
     # expecting a ./config directory with ytt-templated files
     # and optionally a list of locations for value files, meaning either actual files or directories
 
-    ${CWD}/sops-secrets-bin-ytt.sh
+    ${CWD}/ytt.sh
 
 elif [ -f kustomization.yaml ]; then
     # this is a kustomize directory
 
-    ${CWD}/sops-secrets-bin-kustomize.sh
+    ${CWD}/kustomize.sh
 
 else
     # this is plain YAMLs
     # merge them all and split the documents into single files
     # if a document is sops-encrypted in a multi-document file, sops cannot decrypt it
 
-    ${CWD}/sops-secrets-bin-plain.sh
+    ${CWD}/plain.sh
 
 fi
